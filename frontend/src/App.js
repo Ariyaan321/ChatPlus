61dc65ab6a2edd2d9a7efbc79dbc8d9cc400a930
Ariyaan <134138177+Ariyaan321@users.noreply.github.com>
2025-01-11 15:13:33 +0530

    frontend messages setup

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Home from './pages/home/Home';
+
+
+export default function App() {
+  return (
+    <>
+      {/* <div className='flex flex-col items-center justify-center w-screen min-h-screen'> */}
+      <div>
+        <Home />
+      </div>
+    </>
+  )
+}
